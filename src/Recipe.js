5e236d96c09ab817ef3cfe5bf86a375ec4da9023
Daniel <dpericich@gmail.com>
2020-07-22 20:37:34 -0500

    Added variable button to show and hide the ingredients list

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './recipe.module.css'
 
 const Recipe = ({calories, title, image, ingredients}) => {
+    const [isSelected, setSelected] = useState(false)
+
+    const showIngredients = () => {
+        setSelected(!isSelected)
+    }
+
+    const toggleIngredientsButtonText = () => {
+        return isSelected ? "Hide Ingredients" : "Show Ingredients"
+    }
+
     return (
         <div className={style.recipe}>
             <h1 className={style.recipieTitle}>{title}</h1>
             <img src={image} alt={title} />
-            <ol>
+            <button onClick={showIngredients}>{toggleIngredientsButtonText()}</button>
+            {isSelected && <ol>
                 {ingredients.map(ingredient => <li>{ingredient.text}</li>)}
-            </ol>
+            </ol> }
             <p className={style.calories}>Calories: {Math.ceil(calories)}</p>
         </div>
     )
